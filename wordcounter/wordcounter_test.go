83a939e59fa23a4f7bc84daeb3a9197ddfa1996f
Kevin Barbary <wandsworth@europe.com>
2021-06-28 10:06:26 +0100

    added more tests

--- wordcounter/wordcounter_test.go
+++ wordcounter/wordcounter_test.go
@@ -18,3 +18,35 @@ func TestIsEmptyWhenNotEmpty(t *testing.T) {
 		t.Errorf("Word store is not empty but isEmpty returned true")
 	}
 }
+
+func TestCounts1(t *testing.T) {
+	NewWordStore()
+	dictionary.add("test")
+	ans := dictionary.counts()
+	if ans["test"] != 1 {
+		t.Errorf("counts = %d, want 1", ans["test"])
+	}
+}
+
+func TestCounts2(t *testing.T) {
+	NewWordStore()
+	dictionary.add("test")
+	dictionary.add("test")
+	ans := dictionary.counts()
+	if ans["test"] != 2 {
+		t.Errorf("counts = %d, want 2", ans["test"])
+	}
+}
+
+func TestCounts3(t *testing.T) {
+	NewWordStore()
+	dictionary.add("test")
+	dictionary.add("thing")
+	dictionary.add("thing")
+	dictionary.add("test")
+	dictionary.add("test")
+	ans := dictionary.counts()
+	if ans["test"] != 3 {
+		t.Errorf("counts = %d, want 3", ans["test"])
+	}
+}
